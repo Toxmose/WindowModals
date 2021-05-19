9c43ebedf6c96ee7d0c18bd405e62515d752876f
toxmose <jeroen.vandroemme@gmail.com>
2021-05-19 23:54:54 +0200

    added eventlisteners

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const modalOpen = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const modalClose = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', modalOpen);
+
+btnCloseModal.addEventListener('click', modalClose);
+overlay.addEventListener('click', modalClose);
