fe747a5f5fea042dc4823ae15d23437153b588db
toxmose <jeroen.vandroemme@gmail.com>
2021-05-20 22:50:13 +0200

    Escape key event added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
+// open and close functions
 const modalOpen = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -15,8 +16,19 @@ const modalClose = function () {
   overlay.classList.add('hidden');
 };
 
+// opening modal when clicking button (use for loop because 3 buttons all have the same class in html)
 for (let i = 0; i < btnShowModal.length; i++)
   btnShowModal[i].addEventListener('click', modalOpen);
 
+// closing modal when clicking close button
 btnCloseModal.addEventListener('click', modalClose);
+
+// closing modal when clicking outside of the modal (= clicking on the overlay)
 overlay.addEventListener('click', modalClose);
+
+// closing the modal when escape key is pushed
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modalClose();
+  }
+});
